b1e38b783ba725f85a5c006f79934a3961382180
Per Arnborg <kontakt@perarnborg.se>
2018-11-05 21:58:38 +0100

    Test with VA authority

--- samples/VanillaJS/public/user-manager-sample.js
+++ samples/VanillaJS/public/user-manager-sample.js
@@ -27,22 +27,13 @@ Oidc.Log.logger = console;
 Oidc.Log.level = Oidc.Log.WARN;
 
 var settings = {
-    authority: 'http://localhost:5000/oidc',
-    client_id: 'js.tokenmanager',
-    redirect_uri: 'http://localhost:5000/user-manager-sample.html',
-    post_logout_redirect_uri: 'http://localhost:5000/user-manager-sample.html',
+    authority: 'XYZ',
+    client_id: 'XYZ',
+    redirect_uri: 'http://localhost:8081/signin-oidc',
     response_type: 'id_token token',
-    scope: 'openid email roles',
-    
-    popup_redirect_uri:'http://localhost:5000/user-manager-sample-popup-signin.html',
-    popup_post_logout_redirect_uri:'http://localhost:5000/user-manager-sample-popup-signout.html',
-    
-    silent_redirect_uri:'http://localhost:5000/user-manager-sample-silent.html',
-    automaticSilentRenew:true,
-    //silentRequestTimeout:10000,
-
-    filterProtocolClaims: true,
-    loadUserInfo: true
+    scope: 'openid profile',
+    automaticSilentRenew: true,
+    silent_redirect_uri: 'http://localhost:8081/silent-refresh-oidc.html'
 };
 var mgr = new Oidc.UserManager(settings);
 
@@ -67,7 +58,7 @@ mgr.events.addSilentRenewError(function (e) {
 mgr.events.addUserLoaded(function (user) {
     console.log("user loaded", user);
     mgr.getUser().then(function(){
-       console.log("getUser loaded user after userLoaded event fired"); 
+       console.log("getUser loaded user after userLoaded event fired");
     });
 });
 
--- samples/VanillaJS/server.js
+++ samples/VanillaJS/server.js
@@ -1,10 +1,9 @@
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-var open = require('open');
 var path = require('path');
 
-var port = 5000;
+var port = 8081;
 var url = "http://localhost:" + port;
 
 var express = require('express');
@@ -21,9 +20,17 @@ app.get("/oidc-client.js", function(req, res){
     res.sendFile(path.join(__dirname, '../../dist/oidc-client.js'));
 });
 
+app.get("/signin-oidc", function(req, res){
+    res.sendFile(path.join(__dirname, 'public/user-manager-sample-callback.html'));
+});
+
+app.get("/silent-refresh-oidc.html", function(req, res){
+    res.sendFile(path.join(__dirname, 'public/user-manager-sample-silent.html'));
+});
+
 var oidc = require('./oidc.js');
 oidc(url, app);
 
 console.log("listening on " + url);
-//open(url);
+
 app.listen(port);
